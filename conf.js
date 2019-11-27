5de0d627e1d654e22eb16820eddc3393b2b51d7d
chen <admin@chen.lan>
2019-11-27 22:29:50 +0800

    增加测试文件

--- conf.js
+++ conf.js
@@ -1,3 +1,5 @@
-moudle.exports = {
-    // var db = ""
+module.exports = {
+    websockIp:"http://192.168.1.108:8545"//本地连接
+    // websockIp:"https://ropsten.infura.io/P6uKp8W1tfTPBVqm1bqO"//测试网连接
+    // websockIp:"https://ropsten.infura.io/P6uKp8W1tfTPBVqm1bqO"
 }
--- index.js
+++ index.js
@@ -4,3 +4,19 @@
 3.合约监控功能
 4.api接口
 */
+
+const Web3 = require('web3');
+const IpConfig = require("./conf");
+
+var web3 = new Web3(IpConfig["websockIp"]);//本地节点链接
+GetEthNumber("0x1423953c941244994c10ab0007c80302a7db9c79");
+//读取eth代币数量
+function GetEthNumber(address) {
+    // const address = '0xb5e3E5D51fCa7150357a46cFD3Dfd303e8f14638';
+    let myeth = 0;//单位eth
+    web3.eth.getBalance(address).then(e => {
+        // console.log(e)
+        myeth = web3.utils.fromWei(e, 'ether');;
+        console.log(myeth);
+    })
+}
