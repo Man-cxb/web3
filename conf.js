f8fb82cf9914a4851cb382106ebfec0b31452e71
unknown <307729748@qq.com>
2019-11-28 23:53:28 +0800

    modify

--- conf.js
+++ conf.js
@@ -1,5 +1,5 @@
 module.exports = {
-    websockIp:"http://192.168.1.108:8545"//本地连接
-    // websockIp:"https://ropsten.infura.io/P6uKp8W1tfTPBVqm1bqO"//测试网连接
-    // websockIp:"https://ropsten.infura.io/P6uKp8W1tfTPBVqm1bqO"
+    // websockIp:"http://192.168.1.108:8545"//本地连接
+    websockIp:"https://ropsten.infura.io/v3/ef34bbe49df548698d7cdba9dc103f9f"//测试网连接
+    // websockIp:"https://mainnet.infura.io/v3/ef34bbe49df548698d7cdba9dc103f9"
 }
